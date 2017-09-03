2af4e3fec7c84e7dcda312fa2d483ecd87836f31
vxhly <904334946@qq.com>
2017-09-03 22:31:09 +0800

    Site updated: 2017-09-03 22:30:51

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Sat Sep 02 2017 22:37:02 GMT+0800 (中国标准时间)
+// build time:Sun Sep 03 2017 22:30:33 GMT+0800 (中国标准时间)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
