00ab225f07f1fd43d022b5695d7587fbd7c3ae2b
vxhly <pengchengou@gmail.com>
2017-09-29 11:30:26 +0800

    Site updated: 2017-09-29 11:29:49

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Mon Sep 11 2017 21:55:26 GMT+0800 (中国标准时间)
+// build time:Fri Sep 29 2017 11:29:40 GMT+0800 (中国标准时间)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
