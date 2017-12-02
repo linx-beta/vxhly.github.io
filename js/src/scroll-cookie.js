770ffeab93afa28b8891a863fa487e7e28080660
vxhly <pengchengou@gmail.com>
2017-12-02 15:44:43 +0800

    Site updated: 2017-12-02 15:43:53

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Sat Nov 18 2017 14:19:12 GMT+0800 (中国标准时间)
+// build time:Sat Dec 02 2017 15:43:30 GMT+0800 (中国标准时间)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
